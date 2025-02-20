64b534e8204272b033d728e5ca6f19fd2d435e85
rruthes <rafaelruthes444@gmail.com>
2025-02-20 07:57:02 -0300

    Initial commit

---
+++ nosso_vetor/NossoVetor.java
@@ -0,0 +1,20 @@
+public class NossoVetor {
+    private int ocupacao;
+    private int[] vetor;
+  
+    public NossoVetor(int tamanho) {
+      vetor = new int[tamanho];
+      ocupacao = 0; //por clareza, ele zera por natureza
+    }
+  
+    public NossoVetor() {
+      this(10);
+    }
+  
+    public void inserir(int i) {
+      if (ocupacao < vetor.length) {
+        vetor[ocupacao++] = i; // como o ++ aparece dps de ocupação, n tem problema pois ele so incrementa após fazer a inserção
+      }
+  
+    }
+  }
