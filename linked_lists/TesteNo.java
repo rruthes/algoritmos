f8d64b260adbac8e05f19b722c191dd38c186e31
rruthes <rafaelruthes444@gmail.com>
2025-03-18 09:19:03 -0300

    imprimindo no1 e no2

--- linked_lists/TesteNo.java
+++ linked_lists/TesteNo.java
@@ -3,5 +3,7 @@ public class TesteNo {
         No no1, no2;
         no1 = new No(5);
         no2 = new No(8);
+        System.out.println("no1: " + no1);
+        System.out.println("no2: " + no2);
     }
 }
